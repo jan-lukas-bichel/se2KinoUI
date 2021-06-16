be2376f2871e4fe8a2aa36fcd82e86b6b15a7de4
Jan <jlbichel@gmail.com>
2021-06-16 11:05:33 +0200

    Neues Werkzeug Erstellt

---
+++ src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlungswerkzeug/BarzahlungsWerkzeug.java
@@ -0,0 +1,12 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.barzahlungswerkzeug;
+
+public class BarzahlungsWerkzeug {
+
+	/**
+	 * Initialisiert dieses Werkzeug. Das initial ausgewählte Datum ist der heutige
+	 * Tag.
+	 */
+	public BarzahlungsWerkzeug() {
+
+	}
+}
